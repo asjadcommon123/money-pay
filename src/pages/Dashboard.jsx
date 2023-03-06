1031d597c0a9b73fb97d233bf2e620cf4d6bd97f
unegbu <unegbuclinton5@gmail.com>
2023-03-06 11:03:10 +0100

    post like functionality

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -33,17 +33,16 @@ const Dashboard = () => {
   };
 
   const increase = () => {
-    setPageState((prev) => prev + 1);
-    console.log(pageState);
     setIsFetching(true);
+    setPageState((prev) => prev + 1);
   };
 
   useEffect(() => {
     if (listener.current) {
       listener.current = false;
-      // setIsloading(true);
       getProfiles(pageState);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const listInnerRef = useRef();
@@ -52,8 +51,8 @@ const Dashboard = () => {
     if (listInnerRef.current) {
       const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
       if (scrollTop + clientHeight === scrollHeight) {
-        increase();
         console.log('Reached bottom');
+        increase();
         setTimeout(() => {
           getProfiles(pageState);
           setIsFetching(false);
@@ -66,7 +65,6 @@ const Dashboard = () => {
   return (
     <div className="relative overflow-auto px-6 flex flex-col items-center pb-24 justify-center pt-20 bg-hero-pattern h-screen ">
       <Notification />
-
       {isLoading ? (
         <Loading />
       ) : (
@@ -78,12 +76,13 @@ const Dashboard = () => {
           {dataItems?.map(({ URL, likes, id }, idx) => {
             return (
               <div key={idx} className="flex justify-center max-w-[786px]  ">
-                <ImageCard src={URL} likes={likes} />
+                <ImageCard src={URL} likes={likes} id={id} />
               </div>
             );
           })}
         </div>
       )}
+
       {isFetching ? <Loading /> : null}
       <DashboardFooter />
     </div>
