17f581b270e37bb983f7f73d4b2069ba306c7145
unegbu <unegbuclinton5@gmail.com>
2023-03-09 17:15:04 +0100

    update infinite scrolling

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import profile from '../images/profile.png';
 
 const Notification = () => {
   const { userData } = useSelector((state) => state.dashboard);
   const { email, balance } = userData;
-
+  const navigate = useNavigate();
+  const checkout = () => {
+    navigate('/checkout');
+  };
   return (
     <div className="fixed w-5/6 top-4 flex bg-white justify-between py-2 px-3 rounded-2xl z-20">
       <div className="rounded-full w-14 h-14 bg-grey border-black border">
@@ -19,7 +23,10 @@ const Notification = () => {
           <span className="text-light-green">R$</span> {balance}
         </p>
       </div>
-      <div className="flex justify-center items-center text-black bg-light-green px-4 text-sm font-semibold rounded-3xl">
+      <div
+        onClick={checkout}
+        className="flex justify-center items-center text-black bg-light-green px-4 text-sm font-semibold rounded-3xl"
+      >
         SACAR
       </div>
     </div>
