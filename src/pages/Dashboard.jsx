272468dae039f40dd58409fc908574a2c3209a21
unegbu <unegbuclinton5@gmail.com>
2023-03-10 09:41:19 +0100

    scroll bug

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import SyncLoader from 'react-spinners/SyncLoader';
 import Dashboardlayout from '../components/Dashboardlayout';
 import ImageCard from '../components/ImageCard';
 import Loading from '../components/loader/Loading';
@@ -18,12 +19,10 @@ const Dashboard = () => {
   );
 
   const dispatch = useDispatch();
-
   const increase = () => {
     dispatch(pageCounter());
     setIsFetching(false);
   };
-
   useEffect(() => {
     if (listener.current) {
       listener.current = false;
@@ -32,25 +31,44 @@ const Dashboard = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleScroll = (e) => {
-    const bottom =
-      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    if (bottom) {
-      setIsFetching(true);
-      setTimeout(() => {
-        increase();
-        dispatch(updateProfilesList(pageNumber));
-      }, 1500);
-      return;
+  // const handleScroll = (e) => {
+  //   console.log(
+  //     e.target.scrollHeight,
+  //     e.target.scrollTop,
+  //     e.target.clientHeight
+  //   );
+  //   const bottom =
+  //     e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+  //   if (bottom) {
+  //     setIsFetching(true);
+  //     setTimeout(() => {
+  //       console.log('reached');
+  //       // increase();
+  //       // dispatch(updateProfilesList(pageNumber));
+  //     }, 1500);
+  //     return;
+  //   }
+  // };
+  const divRef = useRef(null);
+  const handleScroll = () => {
+    const container = divRef.current;
+    const scrollTop = container.scrollTop;
+    const clientHeight = container.clientHeight;
+    const scrollHeight = container.scrollHeight;
+    const isAtBottom = scrollTop + clientHeight + 500 >= scrollHeight - 30;
+    if (isAtBottom && !isLoading) {
+      increase();
+      dispatch(updateProfilesList(pageNumber));
     }
   };
 
   return (
     <Dashboardlayout>
       {isLoading ? (
-        <Loading />
+        <SyncLoader color="#fff" loading={true} size={10} />
       ) : (
         <div
+          ref={divRef}
           onScroll={handleScroll}
           className="layout mt-10 mb-24 overflow-auto"
         >
